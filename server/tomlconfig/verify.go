0097c3289e199204ff44a318651b9a5e81b338a9
banaag <banaag@google.com>
2020-07-06 18:08:13 -0700

    Updating the toml to include ACME related entries.

--- server/tomlconfig/verify.go
+++ server/tomlconfig/verify.go
@@ -111,9 +111,11 @@ func (c *SXGConfig) verify() error {
 	if err := verifyURL(c.ValidityURL); err != nil {
 		errs = multierror.Append(errs, wrapError("ValidityURL", err))
 	}
-
 	if err := c.Cert.verify(); err != nil {
 		errs = multierror.Append(errs, wrapError("Cert", err))
+	}
+	if err := c.ACME.verify(); err != nil {
+		errs = multierror.Append(errs, wrapError("ACME", err))
 	}
 
 	return errs.ErrorOrNil()
@@ -127,6 +129,30 @@ func (c *SXGCertConfig) verify() error {
 	}
 	if c.KeyFile == "" {
 		errs = multierror.Append(errs, wrapError("KeyFile", errEmpty))
+	}
+
+	return errs.ErrorOrNil()
+}
+
+func (c *SXGACMEConfig) verify() error {
+	var errs *multierror.Error
+
+	if !c.Enable {
+		return nil
+	}
+
+	if c.DiscoveryURL == "" {
+		errs = multierror.Append(errs, wrapError("DiscoveryURL", errEmpty))
+	}
+	if err := verifyURL(c.DiscoveryURL); err != nil {
+		errs = multierror.Append(errs, wrapError("DiscoveryURL", err))
+	}
+	if c.Email == "" {
+		errs = multierror.Append(errs, wrapError("Email", errEmpty))
+	}
+
+	if c.HTTPWebRootDir == "" && c.HTTPChallengePort == 0 && c.TLSChallengePort == 0 && c.DNSProvider == "" {
+		errs = multierror.Append(errs, wrapError("at least one of HTTPWebRootDir, HTTPChallengePort, TLSChallengePort or DNSProvider has to be non-empty", errEmpty))
 	}
 
 	return errs.ErrorOrNil()
